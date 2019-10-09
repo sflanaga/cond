b2d750f0d2107e23e46e07a8013c6688ea5756dd
Steve Flanagan <stevemflanagan@gmail.com>
2019-10-09 10:34:11 -0500

    queue with conditions working

--- src/cond.rs
+++ src/cond.rs
@@ -2,6 +2,7 @@
 use std::sync::{Arc, Mutex, Condvar};
 use std::thread;
 use std::collections::LinkedList;
+use std::time::Duration;
 
 
 struct InnerQ<T> {
@@ -14,7 +15,10 @@ struct InnerQ<T> {
 #[derive(Clone)]
 struct Requeue<T> {
     tqueue: Arc<Mutex<InnerQ<T>>>,
+    cond_more: Arc<Condvar>,
+    cond_hasroom: Arc<Condvar>,
 }
+
 
 impl<T> Requeue<T> {
     pub fn new(max_waits: usize, limit_: usize) -> Requeue<T> {
@@ -26,26 +30,61 @@ impl<T> Requeue<T> {
                     max_waiters: max_waits,
                     limit: limit_,
             })),
+            cond_more: Arc::new(Condvar::new()),
+            cond_hasroom: Arc::new(Condvar::new()),
         }
     }
     pub fn push(&mut self, item: T) {
         let mut l = self.tqueue.lock().unwrap();
+        while l.limit > 0 && l.queue.len() >= l.limit {
+            l = self.cond_hasroom.wait(l).unwrap();
+        }
         l.queue.push_front(item);
+
+        self.cond_more.notify_one();
     }
-    pub fn pop(&mut self) -> Option<T> {
+    pub fn pop(&mut self) -> T {
         let mut l = self.tqueue.lock().unwrap();
-        l.queue.pop_back()
+        l.curr_waiters += 1;
+        while l.queue.len() < 1 {
+            l = self.cond_more.wait(l).unwrap();
+        }
+        let res =l.queue.pop_back().unwrap();
+        if l.limit > 0 {
+            self.cond_hasroom.notify_one();
+            l.curr_waiters -= 1;
+        }
+        res
+    }
+    pub fn waiters(& self,) -> usize {
+        let mut l = self.tqueue.lock().unwrap();
+        l.curr_waiters
     }
 }
 
 fn main() {
 
-    let mut q:Requeue<String> = Requeue::new(4, 10);
-    q.push("dog".to_string());
+    let mut q:Requeue<Option<String>> = Requeue::new(4, 10);
+    q.push(Some("dog".to_string()));
     let mut q_c = q.clone();
     let h = thread::spawn(move|| {
-        println!("other thread: {}", q_c.pop().unwrap());
+        loop {
+            match q_c.pop() {
+                None => break,
+                Some(s) => {}, //println!("other thread {}", s),
+            }
+        }
     });
+    for i in 0 .. 10_000_000 {
+        let s = i.to_string();
+        //println!("waiters {}", q.waiters());
+        q.push(Some(s));
+        //thread::sleep(Duration::from_millis(1000));
+
+    }
+    println!("sending None and DONE");
+    q.push(None);
+
     h.join();
 
 }
